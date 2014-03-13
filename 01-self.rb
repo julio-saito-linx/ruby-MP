4dff9053a29fabdd975f2c20a1c8e7b6f4f1a857
Julio Makdisse Saito <saitodisse@gmail.com>
2014-03-12 23:37:26 -0300

    more than myself

--- 01-self.rb
+++ 01-self.rb
@@ -3,7 +3,10 @@
 p 'self global: ' << self.to_s
 
 class User
-	def initialize
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
 		p 'User self: ' << self.to_s
 	end
 
@@ -11,7 +14,14 @@ class User
 		istheSame = (object == self).to_s
 		p 'compare, istheSame? ' << istheSame
 	end
+
+	def say_hello
+		'Hi! My name is ' << name # implicitly 'self' on "name() method"
+	end
 end
 
-u = User.new
+u = User.new("Julio")
 u.compare(u)
+
+p 'The name is ' << u.name # attr_accessor :name
+p u.say_hello
